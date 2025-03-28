91c3de46fd4d02cd4d6b218c8ceadfb3a94b7f1e
Damien Palm <palm.damien.pro@gmail.com>
2025-03-28 03:23:12 +0100

    integration de la User Story 4 -affichage du prénom de la personne connecté -ajout style Homepage.css

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import { useUserData } from "../hooks/useUserData";
+import "../styles/Homepage.css";
 
 function Homepage() {
+  const [userId] = useState(18);
+  const { userData, loading, error } = useUserData(userId);
+  console.log(userData, loading, error);
+
+  const firstName = userData?.data?.userInfos?.firstName;
+
   return (
     <>
       <header className="header">
@@ -9,6 +18,18 @@ function Homepage() {
       </header>
       <main className="main">
         <Sidebar />
+        <main className="main__profilSection">
+          <section className="main__profilSection__profilWrapper">
+            <div className="main__profilSection__profilWrapper__helloSection">
+              <h1 className="main__profilSection__profilWrapper__helloSection__userName">
+                Bonjour <span>{firstName}</span>
+              </h1>
+              <p className="main__profilSection__profilWrapper__helloSection__description">
+                Félicitation vous avez explosé vos objectifs hier 👏
+              </p>
+            </div>
+          </section>
+        </main>
       </main>
     </>
   );
