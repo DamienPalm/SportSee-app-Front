a8b8b1082e238edfc3d6d4da962fde39f9b5a8dc
Damien Palm <palm.damien.pro@gmail.com>
2025-03-28 12:05:48 +0100

    integration de la duree moyenne des sessions -creation du composant AverageSessionsLineChart.jsx -creation du style AverageSessionsLineChart.css -ajout a la page Homepage.jsx

---
+++ src/components/AverageSessionsLineChart.jsx
@@ -0,0 +1,90 @@
+import "../styles/AverageSessionsLineChart.css";
+import {
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+function CustomTickFormatter(day) {
+  switch (day) {
+    case 1:
+      return "L";
+
+    case 2:
+      return "M";
+
+    case 3:
+      return "M";
+
+    case 4:
+      return "J";
+
+    case 5:
+      return "V";
+
+    case 6:
+      return "S";
+
+    case 7:
+      return "D";
+
+    default:
+      return day;
+  }
+}
+
+function CustomizedTooltip({ active, payload }) {
+  if (active && payload && payload.length) {
+    return (
+      <div className="customTooltip">
+        <p className="customTooltip__payload">{payload[0].value + "min"}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+function AverageSessionsLineChart({ data }) {
+  return (
+    <div className="main__profilSection__profilWrapper__averageSessionsLineChart">
+      <h2 className="main__profilSection__profilWrapper__averageSessionsLineChart__title">
+        Durée moyenne des sessions
+      </h2>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={data}
+          margin={{ top: 77, right: 0, left: 0, bottom: 20 }}
+        >
+          <XAxis
+            dataKey="day"
+            axisLine={false}
+            tickLine={false}
+            tickFormatter={CustomTickFormatter}
+            tick={{ fill: "white" }}
+            padding={{ left: 20, right: 20 }}
+          />
+          <YAxis hide domain={["dataMin-10", "dataMax+10"]} />
+          <Line
+            type="natural"
+            dataKey="sessionLength"
+            strokeWidth={2}
+            stroke="white"
+            activeDot={{
+              r: 4,
+              strokeWidth: 8,
+              stroke: "rgba(255, 255, 255, 0.2)",
+            }}
+            dot={false}
+          />
+          <Tooltip content={CustomizedTooltip} />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default AverageSessionsLineChart;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,15 +5,18 @@ import { useUserData } from "../hooks/useUserData";
 import "../styles/Homepage.css";
 import ActivityBarChart from "../components/ActivityBarChart";
 import { useUserActivity } from "../hooks/useUserActivity";
+import AverageSessionsLineChart from "../components/AverageSessionsLineChart";
+import { useUserAverageSessions } from "../hooks/useUserAverageSessions";
 
 function Homepage() {
   const [userId] = useState(18);
-  const { userData, loading, error } = useUserData(userId);
-  console.log(userData, loading, error);
+  const { userData } = useUserData(userId);
   const { userActivity } = useUserActivity(userId);
+  const { userAverageSessions } = useUserAverageSessions(userId);
 
   const firstName = userData?.data?.userInfos?.firstName;
   const activity = userActivity?.data?.sessions;
+  const averageSessions = userAverageSessions?.data?.sessions;
 
   return (
     <>
@@ -33,6 +36,9 @@ function Homepage() {
               </p>
             </div>
             <ActivityBarChart data={activity} />
+            <section className="main__profilSection__profilWrapper__cardWrapper">
+              <AverageSessionsLineChart data={averageSessions} />
+            </section>
           </section>
         </main>
       </main>
