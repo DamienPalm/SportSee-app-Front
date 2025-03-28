91a23c550491402243fbc101f9866674c1e56957
Damien Palm <palm.damien.pro@gmail.com>
2025-03-28 02:25:08 +0100

    creation du service Api.js permet de recuperer les donnees des utilisateurs depuis le backend local grace aux fonctions asynchrones

---
+++ src/services/api.js
@@ -0,0 +1,44 @@
+export async function fetchUserData(userId) {
+  const response = await fetch(`http;//localhost:3000/user/${userId}`);
+  if (!response.ok) {
+    throw new Error("Erreur lors de la récupération des données utilisateurs");
+  } else {
+    return response.json();
+  }
+}
+
+export async function fetchUserActivity(userId) {
+  const response = await fetch(`http;//localhost:3000/user/${userId}/activity`);
+  if (!response.ok) {
+    throw new Error(
+      "Erreur lors de la récupération des données de l'activité des utilisateurs"
+    );
+  } else {
+    return response.json();
+  }
+}
+export async function fetchUserAverageSession(userId) {
+  const response = await fetch(
+    `http;//localhost:3000/user/${userId}/average-sessions`
+  );
+  if (!response.ok) {
+    throw new Error(
+      "Erreur lors de la récupération des données des sessions moyennes des utilisateurs"
+    );
+  } else {
+    return response.json();
+  }
+}
+
+export async function fetchUserPerformance(userId) {
+  const response = await fetch(
+    `http;//localhost:3000/user/${userId}/performance`
+  );
+  if (!response.ok) {
+    throw new Error(
+      "Erreur lors de la récupération des données des performances des utilisateurs"
+    );
+  } else {
+    return response.json();
+  }
+}
